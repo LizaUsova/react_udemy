5ff18ca39c0364a535120ac5d4acb059fd091218
Liza Usova <Liza.usova@gmail.com>
2025-05-29 12:26:38 +0300

    array of objects

---
+++ src/14-array-of-objects/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Persons from "./components/Persons.jsx";
+
+
+
+function App() {
+
+
+    return (
+        <div className="container my-4">
+            <Persons />
+        </div>
+    )
+}
+
+export default App
